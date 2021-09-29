89b4e32a0584e07d5049085e5f6f25ea5436127d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-09-29 12:11:40 +0200

    lazy loading

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'books' }
+  // { path: '', pathMatch: 'full', redirectTo: 'books' }
+  { path: 'books',
+    loadChildren: () => import('./books/books.module')
+      .then(m => m.BooksModule) }
 ];
 
 @NgModule({
