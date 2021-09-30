3163ee372a3f3fd93e73819302cdce879b05961a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-09-30 13:38:23 +0200

    rxjs: komischer lade-indikator

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, mergeMap, shareReplay, switchMap } from 'rxjs/operators';
+import { map, mergeMap, shareReplay, startWith, switchMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -12,7 +12,9 @@ export class BookDetailsComponent {
 
   book$ = this.router.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn!)),
+    switchMap(isbn => this.bs.getSingleBook(isbn!).pipe(
+      startWith(null)
+    )),
   );
 
   // ANTI PATTERN!
